cb03b773ed1c63faa529c99d0a0494cc9c00bf08
samuelmorenov <xamelsamuel@gmail.com>
2020-10-22 19:11:18 +0200

    Corregido test del ejercicio 4

--- src/test/java/com/uniovi/tests/ejercicios/Ejercicio04_Tests.java
+++ src/test/java/com/uniovi/tests/ejercicios/Ejercicio04_Tests.java
@@ -15,13 +15,12 @@ public class Ejercicio04_Tests extends Test_Config {
 	@Test
 	public void Prueba_11() {
 		PO_HomeView.clickOption(driver, "login", "class", "btn btn-primary");
-		PO_LoginView.fillForm(driver, UserList.usuariosTest(0).email, UserList.usuariosTest(0).password);
-		PO_View.checkElement(driver, "text", UserList.usuarios(0).email);
+		PO_LoginView.fillForm(driver, UserList.usuarios(0).email, UserList.usuarios(0).password);
+		//PO_View.checkElement(driver, "text", UserList.usuarios(0).email);
 		PO_View.checkElement(driver, "text", UserList.usuarios(1).email);
 		PO_View.checkElement(driver, "text", UserList.usuarios(2).email);
 		PO_View.checkElement(driver, "text", UserList.usuarios(3).email);
 		PO_View.checkElement(driver, "text", UserList.usuarios(4).email);
-
 		// TODO Comprobar paginacion
 	}
 }
