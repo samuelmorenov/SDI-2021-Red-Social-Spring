2b43c98398a37255894ca0b9cfc807f36e556c6e
samuelmorenov <xamelsamuel@gmail.com>
2020-10-28 17:46:49 +0100

    Implementado test de ejercicio 9

--- src/test/java/com/uniovi/tests/ejercicios/Ejercicio09_Tests.java
+++ src/test/java/com/uniovi/tests/ejercicios/Ejercicio09_Tests.java
@@ -1,8 +1,13 @@
 package com.uniovi.tests.ejercicios;
 
-import static org.junit.Assert.fail;
-
 import org.junit.Test;
+
+import com.uniovi.services.data.UserList;
+import com.uniovi.tests.pageobjects.PO_HomeView;
+import com.uniovi.tests.pageobjects.PO_Invitation;
+import com.uniovi.tests.pageobjects.PO_LoginView;
+import com.uniovi.tests.pageobjects.PO_PrivateView;
+import com.uniovi.tests.pageobjects.PO_View;
 
 public class Ejercicio09_Tests extends BaseTests {
 	/**
@@ -11,6 +16,15 @@ public class Ejercicio09_Tests extends BaseTests {
 	 */
 	@Test
 	public void Prueba_19() {
-		fail("Not yet implemented");
+		String amigo1 = PO_Invitation.enviarPeticionCuentaNueva(driver, 1);
+		String amigo2 = PO_Invitation.enviarPeticionCuentaNueva(driver, 1);
+		PO_HomeView.clickOption(driver, "login", "class", "btn btn-primary");
+		PO_LoginView.fillForm(driver, UserList.usuarios(1).email, UserList.usuarios(1).password);
+		PO_PrivateView.accederPagina(driver, "friends-menu", "/friend/invitationlist");
+		PO_HomeView.clickId(driver, "acceptButton" + amigo1);
+		PO_HomeView.clickId(driver, "acceptButton" + amigo2);
+		PO_PrivateView.accederPagina(driver, "friends-menu", "/friend/friendlist");
+		PO_View.checkElement(driver, "text", amigo1);
+		PO_View.checkElement(driver, "text", amigo2);
 	}
 }
